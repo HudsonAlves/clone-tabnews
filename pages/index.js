47a96f5b63d3ca01b177176b9cf82fdcc7fa1fa0
Hudson Da Silva Alves <hud.alves@gmail.com>
2025-11-17 21:52:08 +0100

    proposito do `site`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Home() {
-  return <h1>Mariana Te amo! Se você me ama, da uma risadinha!!</h1>;
+  return (
+    <p>
+      proposito do site - Um site com as perguntas mais frequentes do
+      catolicismo perfil do usuário - Católicos que não se aprofundaram na fe
+      feito - criar lista de perguntas que vao receber upvote quando você abrir
+      e mais upvote se você ficar com ela aberta por um tempo protegido - dados
+      pessoais dos usuarios repetido - ;
+    </p>
+  );
 }
 
 export default Home;
